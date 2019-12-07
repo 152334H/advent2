39a73e04231ac8f53cf2a73378b6ef832a65a7c6
152334H <a@DESKTOP-2TND5IO.localdomain>
2019-12-07 16:46:33 +0800

    edit

--- f.py
+++ f.py
@@ -1,21 +1,20 @@
-with open("i.6") as f: s = map(lambda s: s.split(')'), f.read().split('\n'))[:-1]
-COM = {}            #tree
+with open("input.txt") as f: s = map(lambda s: s.split(')'), f.read().split('\n'))[:-1]
 loc = {'COM':()}    #path of root to node
-ptr = {'COM':COM}   #pointer to node
+ptr = {'COM':{}}   #pointer to node
 while len(s): #s is gradually shrunk
     i = 0
     for orb in s[:]: #make a copy
         if orb[0] not in loc: i+=1
         else: #if orbit can be attached to cur tree
             loc[orb[1]] = loc[orb[0]]+(orb[1],)
-            ptr[orb[1]] = COM[orb[1]] = {}
+            ptr[orb[1]] = ptr[orb[0]][orb[1]] = {}
             del s[i]
 print sum(len(v) for v in loc.values())
 san, you = loc['SAN'], loc['YOU']
 while san[i] == you[i]: i += 1 #i starts at 0
 print len(san)-i+len(you)-i-2 #-2 to exclude SAN & YOU
-
 '''
 from networkx import *
-for a in (lambda g:(sum(len(ancestors(g,h)) for h in g.node),len(shortest_path(g.to_undirected(),'YOU','SAN'))-3))(read_edgelist('i.6','#',')',DiGraph())):print a
+#for a in (lambda g:(sum(len(ancestors(g,h)) for h in g.node),len(shortest_path(g.to_undirected(),'YOU','SAN'))-3))(read_edgelist('input.txt','#',')',DiGraph())):print a
+g = read_edgelist('input.txt','#',')',DiGraph())
 '''
