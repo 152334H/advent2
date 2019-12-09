d4f808f25a184fd19556f8f8644379eb7ceba137
152334H <54623771+152334H@users.noreply.github.com>
2019-12-10 07:50:33 +0800

    count

---
+++ i.py
@@ -0,0 +1,4 @@
+import aoc, e
+s = aoc.sread('i.9', int, ',')
+print e.runtime(s).send(1)
+print e.runtime(s).send(2)
--- j.py
+++ j.py
@@ -1,4 +1,1 @@
-import aoc, e
-s = aoc.sread('i.9', int, ',')
-print e.runtime(s).send(1)
-print e.runtime(s).send(2)
+import aoc
