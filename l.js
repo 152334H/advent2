550bd8c6d4bf31586c1f32992072ba7b3c3bdc89
152334H <a@HostComputer.wifi.lab.local>
2019-12-13 11:58:29 +0800

    formatting

--- l.js
+++ l.js
@@ -1,19 +1,19 @@
 let s = require('./aoc').sreadlines('i.12').map(s => s.match(/[-\d]+/g).map(Number));
-let v = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+let v = [...Array(4)].map(x => Array(3).fill(0))
 let T = Array(3).fill(null);
-const init = new Set()
-for (let i = 0; i < 3; i++)
-  init.add(i+String(s.map(l => l[i])));
-init.add('v'+String([0,0,0,0]))
+
+const init = new Set().add('v'+String(Array(4).fill(0)));
+for (let i = 0; i < 3; i++) init.add(i+String(s.map(l => l[i])));
+
 for (let i = 0; T.includes(null); i++) {
 	if (i === 1000) {
-		const f = l => l.map(Math.abs).reduce((_,$) => $+_), ke = v.map(f), pe = s.map(f);
+		const f = l => l.map(Math.abs).reduce((_,$) => $+_);
+		const ke = v.map(f), pe = s.map(f);
 		let total = 0;
-		for (let j = 0; j < 4; j++) {
-			total += ke[j]*pe[j];
-		}
+		for (let j = 0; j < 4; j++) total += ke[j]*pe[j];
 		console.log(total);
 	}
+
 	for (let me = 0; me < 4; me++) {
 		for (let oth = 0; oth < 4; oth++) {
 			if (oth === me) continue;
@@ -26,10 +26,12 @@ for (let i = 0; T.includes(null); i++) {
 	for (let me = 0; me < 4; me++) {
 		for (let ax = 0; ax < 3; ax++) s[me][ax] += v[me][ax];
 	}
+
 	for (let ax = 0; ax < 3; ax++) {
-		if (T[ax] === null){
-      if (init.has('v'+String(v.map(l => l[ax]))) && init.has(ax+String(s.map(l => l[ax])))) T[ax] = i+1;
-    }
+		if (T[ax] === null) {
+		    if (init.has('v'+String(v.map(l => l[ax]))) && init.has(ax+String(s.map(l => l[ax]))))
+			T[ax] = i+1;
+		}
 	}
 }
 const gcd = (a,b) => b ? gcd(b, a%b) : a;
