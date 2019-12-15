156c90d17acbabc65c316f896bba7fa46defb394
152334H <54623771+152334H@users.noreply.github.com>
2019-12-15 19:11:28 +0800

    commenting,speed,inputs

--- aoc.py
+++ aoc.py
@@ -1,4 +1,5 @@
 def sread(name, t=str, div=None):
+    '''read file NAME, split it with DIV and convert it to type T'''
     with open(name) as f: s = f.read()
     if s[-1] == '\n': s = s[:-1]
     if div != None: s = s.split(div)
@@ -9,6 +10,7 @@ def sread(name, t=str, div=None):
     return s
 
 def sreadlines(name, t=str, div=None):
+    '''sread, but split across newlines first'''
     s = sread(name).split('\n')
     if div != None:
         s = map(lambda l: l.split(div), s)
@@ -20,6 +22,7 @@ def sreadlines(name, t=str, div=None):
     return s
 
 def toGrid(d, MAP):
+    '''converts a dictionary grid D to a printable representation using the value-to-char mapping MAP'''
     l = d.keys()
     xma, xmi = max(l)[0], min(l)[0]
     l = [t[1] for t in l]
@@ -32,6 +35,7 @@ def toGrid(d, MAP):
     return s
 
 def binsearch(f, ma): #thus far, only tested on `ma=2**i`
+    '''binary search across an arbitrary function F(x), with a maximal value of MA (and a minimal value of 0)'''
     c = ma
     sign = True
     i = len(bin(ma))-2
@@ -42,7 +46,13 @@ def binsearch(f, ma): #thus far, only tested on `ma=2**i`
     if sign: c-=1 #if binary search ended off-by-one
     return c
 
-def adj(x,y): return [(x,y+1), (x,y-1), (x+1,y), (x-1,y)]
+def adj(x,y):
+    '''returns an array of the 4 (x,y) coordinates adjacent to the input coordinate'''
+    return [(x,y+1), (x,y-1), (x+1,y), (x-1,y)]
+
+def back_adj(adj_c, i):
+    '''given a coordinate from adj(), get back the original (x,y)'''
+    return adj(*adj_c)[{0:1, 1:0, 2:3, 3:2}[i]]
 
 def distance_map(start, grid, func, optional=lambda:0):
     '''returns a dict containing a grid's (coord:manhatten-distance-to-start) pairings
@@ -51,14 +61,14 @@ def distance_map(start, grid, func, optional=lambda:0):
     `optional()` runs once for each point on the grid'''
     from collections import defaultdict as dd
     d = dd(lambda: 0)   #start counting distance from 0
-    border = [start] #an expanding border (coord, traversal-history) of the traversed portion of the grid.
+    border = [start]    #an expanding border (coord, traversal-history) of the traversed portion of the grid.
     for coord in border:
         adjacent = adj(*coord)  #list of directly (taxicab) adjacent coordinates
         for direct in range(4): #loop numerically, rather than by *adj(), to provide direction info to func()
             adj_c = adjacent[direct]    #unnecessary variable for clarity
             if adj_c not in d:
                 if func(adj_c, grid, direct):
-                    border.append(adj_c)   #update border
+                    border.append(adj_c)    #update border
                     d[adj_c] = d[coord]+1   #add to distance dict
-        optional() #do something after each iteration
+        optional()  #do something after each iteration
     return d
--- o.py
+++ o.py
@@ -3,15 +3,11 @@ from collections import defaultdict as dd, deque as dq
 s = aoc.sread('i.15', int, ',')
 grid = dd(lambda: -1)
 WALKABLE = (1,2)
-def checkMove(adj_c, grid, *_):
-    return grid[adj_c] in WALKABLE
-counter = 0
-def incCount(v=1):
-    global counter
-    counter+=v
-def plotCheckMove(adj_c, grid, move, internal_border=dq([e.runtime(s).throw(RuntimeError)])): #note that the dq() is only spawned on the first function call
-    if counter:
-        incCount(-1)
+def checkMove(adj_c, grid, *_): return grid[adj_c] in WALKABLE
+def plotCheckMove(adj_c, grid, move, internal_border=dq([e.runtime(s).throw(RuntimeError)]), prev=[(0,0)]): #note that the dq() is only spawned on the first function call
+    back = aoc.back_adj(adj_c, move)
+    if prev[0] != back:
+        prev[0] = back
         internal_border.popleft()
     t = internal_border[0]
     t = dd(lambda:0, t[0].items()), t[1], t[2] #deepcopy the intcode runtime
@@ -23,7 +19,7 @@ def plotCheckMove(adj_c, grid, move, internal_border=dq([e.runtime(s).throw(Runt
         return True
     return False
 
-dmap = aoc.distance_map((0,0), grid, plotCheckMove, incCount)
-opos = [k for k in grid if grid[k] == 2][0]
-print dmap[opos]
-print max(aoc.distance_map(opos, grid, checkMove).values())
+dmap = aoc.distance_map((0,0), grid, plotCheckMove) #build & measure map distance via BFS
+opos = [k for k in grid if grid[k] == 2][0] #position of oxygen
+print dmap[opos]    #part 1: distance of opos to centre
+print max(aoc.distance_map(opos, grid, checkMove).values()) #part 2: measure furthest point from opos
