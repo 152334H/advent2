a1015cc97471c9ccce9b9343a52b942153b7f347
152334H <54623771+152334H@users.noreply.github.com>
2019-12-25 14:39:18 +0800

    move

--- i.js
+++
@@ -1,4 +0,0 @@
-const e = require('./e')
-const s = require('./aoc').sread('i.9', Number, ',')
-console.log(e.runtime(s).next(1).value)
-console.log(e.runtime(s).next(2).value)
--- i.py
+++
@@ -1,4 +0,0 @@
-import aoc, e
-s = aoc.sread('i.9', int, ',')
-print e.runtime(s).send(1)
-print e.runtime(s).send(2)
